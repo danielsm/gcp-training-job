c6e2c8d79a7be6d15209ba6b24b8059af7e43723
Daniel Moraes <danielmoares14@gmail.com>
2025-02-24 16:04:00 -0300

    jit compile true and batch_size

--- trainer/task.py
+++ trainer/task.py
@@ -362,7 +362,7 @@ class ContrastiveModel(keras.Model):
         encoder = keras.Model.from_config(encoder_config, custom_objects=custom_objects)  # Restore encoder model
         return cls(encoder=encoder, **config)
     
-    @tf.function(jit_compile=False)
+    @tf.function(jit_compile=True)
     def train_step(self, data):
         
 
@@ -405,7 +405,7 @@ if __name__ == "__main__":
     parser.add_argument("--input_dir", type=str, default="gs://dataset-dcts-fm/TFRecords", help="The TFRecords directory.") # Esse caminho vai funcionar?
     parser.add_argument("--output_dir", type=str, default="gs://dataset-dcts-fm/output-tpu", help="The directory to save the model and checkpoint.")
     parser.add_argument("--resume_from", type=str, default=None, help="Path to a saved model to resume training.")
-    parser.add_argument("--learning_rate", type=float, default=1e-4, help="Learning rate for training.")
+    parser.add_argument("--learning_rate", type=float, default=5e-4, help="Learning rate for training.")
     parser.add_argument("--batch_size", type=int, default=2048, help="Batch size for training.")
     parser.add_argument("--patch_size", type=int, default=8, help="Size of the patch the image is divided.")
     parser.add_argument("--num_patches", type=int, default=784, help="Number of patches resulted from the image.")
@@ -420,7 +420,7 @@ if __name__ == "__main__":
 
     print("Arguments:", args.input_dir, args.output_dir, args.resume_from, args.learning_rate, args.batch_size, args.patch_size, args.num_patches, args.num_blocks, args.num_heads, args.projection_dim, args.temperature, args.tpu)
     try:
-        cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(args.tpu)
+        cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu="")
         tf.config.experimental_connect_to_cluster(cluster_resolver)
         tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
         strategy = tf.distribute.TPUStrategy(cluster_resolver)
